ae87c699fc063d7501721c25a7ed26d561682e19
MR.Tajeddin <mr.tajeddin@gmail.com>
2021-04-21 21:48:47 +0430

    Create os.py

---
+++ os.py
@@ -0,0 +1,8 @@
+#s.py
+import os
+os.environ
+#os.environ['PATH']
+#os.environ['LANG']
+os.makedirs('/Users/alitaj/Desktop/dir2/dir3')
+os.removedirs('/Users/alitaj/Desktop/dir2/dir3')
+#os.makedirs('/Users/alitaj/Desktop/dir1/dir3')
