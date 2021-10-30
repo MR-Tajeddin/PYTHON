66f315d2e034b02f9a2a929b4dc3ad4ef6a1d825
MR.Tajeddin <mr.tajeddin@gmail.com>
2021-10-30 17:25:41 +0330

    Create math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+#math.py
+from textwrap import indent
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.defchararray import index
+
+x = np.linspace(0,300,3000)
+y =-x**2+100*x+60000
+plt.plot(x,y)
+
+ymax = np.max(y)
+index = np.argmax (y)
+
+plt.plot(x[index],ymax, 'or')
+
+xmax = np.round (x[index],0)
+s = 'Max Point = '+str (xmax)
+plt.text (x[index],ymax+1000, s)
+plt.show ()
+#print (xmax)
