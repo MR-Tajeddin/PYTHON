68b75488f450c7393bdc2c0a5c5e4900fc9ea1ce
MR.Tajeddin <mr.tajeddin@gmail.com>
2022-05-29 07:49:53 +0430

    Update os.py

--- os.py
+++ os.py
@@ -1,8 +1,11 @@
 #s.py
 import os
+
 os.environ
+
 #os.environ['PATH']
 #os.environ['LANG']
+
 os.makedirs('/Users/alitaj/Desktop/dir2/dir3')
 os.removedirs('/Users/alitaj/Desktop/dir2/dir3')
 #os.makedirs('/Users/alitaj/Desktop/dir1/dir3')
