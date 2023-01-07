3eab56dc00027acba2cbad2ad54e719fe90229f3
MR.Tajeddin <mr.tajeddin@gmail.com>
2023-01-07 08:39:14 +0330

    Update Cryptocurrencies.py

--- Cryptocurrencies.py
+++ Cryptocurrencies.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import scipy.stats as stt
+import scipy.stats as stt 
 
 URL = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=ETH&market=USD&apikey=RV1XK080XUBTH2EG&datatype=csv'
 Columns = ['timestamp', 'open (USD)', 'high (USD)', 'low (USD)', 'close (USD)', 'volume']
